f24102382c04e47517cefdc13d3bf1e83549e312
DenisVolhonsky <dvolhonsky@gmail.com>
2017-09-21 11:08:02 +0300

    drop menu

--- class/js10/script.js
+++ class/js10/script.js
@@ -1,6 +1,5 @@
 var navDiv = document.querySelectorAll('nav div'); // массив
 
-// div.onclick = fillDiv;
 for(var i=0; i<navDiv.length; i++) {
 	navDiv[i].onmouseenter = showMenu;
 	navDiv[i].onmouseleave = hideMenu;
@@ -10,6 +9,7 @@ for(var i=0; i<navDiv.length; i++) {
 function showMenu() {
 	this.getElementsByTagName('ul')[0].style.display='block';
 }
+
 
 function hideMenu() {
 	this.getElementsByTagName('ul')[0].style.display='none'; 
