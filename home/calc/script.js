95e7e7500115ab22fa7b3abe42fafcc149705e58
DenisVolhonsky <dvolhonsky@gmail.com>
2017-09-25 17:31:06 +0300

    beta without eval

--- home/calc/script.js
+++ home/calc/script.js
@@ -20,14 +20,78 @@ document.onclick = function(event) {
 	else var data = document.getElementById('inp').value += event.target.value; //иначе вывадим в input
 
 
-	 ///////// если eval :-) ////////////////////////
-	 document.getElementById('result').onclick = function (){
-	 	document.getElementById('inp').value = eval(data);
-	 }
+	 // ///////// если eval :-) ////////////////////////
+
+	 // document.getElementById('result').onclick = function (){
+	 // 	document.getElementById('inp').value = eval(data);
+	 // }
 	
+	
+	var rez;
+
+	for(i=0; i<data.length; i++) {  
+//////////// действия в скобках////////////////////////
+		if(data[i] =='(' && data[i+4] ==')') {  
+			if(data[i+2]== '*') {
+				rez+=data[i+1]*data[i+3];  
+			}
+			else if(data[i+2]== '/') {	
+				rez+=data[i+1]/data[i+3];
+			}
+			else if(data[i+2]== '+') {		
+				rez+=data[i+1]+data[i+3];
+			}
+			else if(data[i+2]== '-') {		
+				rez+=data[i+1]-data[i+3];
+			}
+		} 
+//////////////////отператоры *,/,+,- //////////////////////
+		else if(data[i] == '*') {   
+			rez+=data[i-1]*data[i+1];
+		}
+		else if (data[i] == '/') {
+			rez+=data[i-1]/data[i+1];
+		}
+		else if (data[i] == '+') {
+			rez+=data[i-1]+data[i+1];
+		}
+		else if (data[i] == '-' && data[i-1] != null) {
+			rez+=data[i-1]-data[i+1];
+		}
+/////////////////////если число отрицательное///////////////////
+		else if (data[i] == '-' && data[i] == null) {
+			rez+=data[i]*(-1);
+		}
+	}
+ 	console.log(rez); 
+}
+	
+  
 
 
+// function mult() {
+// 	if (data[i]=='*') {
+// 		rez+=data[i-1]*data[i+1];
+// 	}
+// }
+
+	
+// function divine() {
+// 	if (data[i]=='/') {
+// 		rez+=data[i-1]/data[i+1];
+// }
 
 
-			
+// function plus() {
+// 	if (data[i]=='+') {
+// 		rez+=data[i-1]+data[i+1];
+// 	}
+// }
+
+// function minus() {
+// 	if (data[i]=='-') {
+// 		rez+=data[i-1]-data[i+1];
+// 	}
+// }	
+
 			
