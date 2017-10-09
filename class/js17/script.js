e4be090d86b46de96eeb2fbdce698278aa5872b8
DenisVolhonsky <dvolhonsky@gmail.com>
2017-10-09 20:19:24 +0300

    upgrade task1,2, added task3

--- class/js17/script.js
+++ class/js17/script.js
@@ -1,8 +1,10 @@
-//https://developer.mozilla.org/ru/docs/Web/API/Element/classList
+// доп. материалы - https://developer.mozilla.org/ru/docs/Web/API/Element/classList
 
-// var div=document.querySelector('.one');
- // div.classList.add('five', 'six'); //добавить класс
+// var div = document.querySelector('.one');
+
+// div.classList.add('five', 'six'); //добавить класс
 // console.log(div.classList);
+
 // div.classList.remove('one'); //удалить класс
 // if(div.classList.contains('main')) //проверка содержит класс 
 // 	console.log('содержит');
@@ -10,36 +12,49 @@
 // div.classList.toggle('main'); //включает и выключает класс повторный вызов
 // div.classList.toggle('main');
 
-// Задача. Добавьте всем изображениям на странице класс bordered в
+// Задача1. Добавьте всем изображениям на странице класс bordered в
 // оформлении которого содержится граница и тень.
-
-// Задача. Добавьте изображениям, по которым кликнул пользователь, класс
+// Задача2. Добавьте изображениям, по которым кликнул пользователь, класс
 // bordered в оформлении которого содержится граница и тень.
+//
+// var img = document.getElementsByTagName('img');
+// 	for(var i=0; i<img.length; i++) {
+// 		img[i].onclick = function() {
+// 			this.classList.toggle('bordered');
+// 		}
+// 	}
 
-var img = document.getElementsByTagName('img');	
-	for(i=0; i<img.length; i++) {
-		img[i].onclick = function() {
-			this.classList.toggle('bordered');
-		}			
+	document.onclick = function(event){
+	event = event||window.event;
+	if(event.target.tagName =='IMG')
+		event.target.classList.toggle('bordered');
 	}
 
-// Задача. Создайте с помощью JS несколько блоков. Четным блокам
+// Задача3. Создайте с помощью JS несколько блоков. Четным блокам
 // добавьте класс bordered. Добавьте на все блоки событие клика. При клике
 // удаляйте класс bordered. Обратите внимание, что происходит если клик
 // осуществляется по элементу без данного класса.
-
-for(var i=0; i<5; i++) {
+for(var i=0; i<10; i++) {
 	var div = document.createElement('div');
-	div.innerHTML=i;
-	if(i%2==0) {
-		div.classList.add('bordered');
-	}
-	document.body.appendChild(div);
+	div.classList.add('sizing');
+	div.innerHTML = i;
+    var elem = document.getElementById('node');
+    elem.appendChild(div);
+	if(i%2==0) div.classList.add('bordered');
 	div.onclick = function() {
-		this.classList.remove('bordered');	
-	}				
+		this.classList.remove('bordered');
+	}
 }
 
+// Задача. Выведите в консоль количество элементов, которые имеют класс bordered.
+document.getElementById('calcBordered').onclick = function() {
+    var count = document.querySelectorAll('.bordered');
+	console.log('На данный момент на странице '+ count.length +' елемент(ов) имеют класс "bordered"' );
+}
+
+
+
+ 
 
 
 
